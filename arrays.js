49b19f2d03b73fe0f4395a6eaa617d1c50c9eb8c
Dale Sublett <djsublett04@gmail.com>
2019-03-26 23:56:47 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,19 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+  ]
+  
+  function addElementToBeginningOfArray(array, element){
+   array = [];
+   array.unshift(element); 
+   return array
+  }
+  
+  function destructivelyAddElementToBeginningOfArray(array, element){ 
+    array = [];
+    array.unshift(element);
+    return array
+    
+  }
