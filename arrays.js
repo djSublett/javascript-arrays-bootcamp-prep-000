1ddb235f2428d2c9af8504114e7e37e2222e826d
Dale Sublett <djsublett04@gmail.com>
2019-03-27 00:35:48 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -42,8 +42,7 @@ function removeElementFromBeginningOfArray(array) {
 function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop();
   return array;
-  array.pop()
-  return array
+  
 }
 
 function removeElementFromEndOfArray(array) {
