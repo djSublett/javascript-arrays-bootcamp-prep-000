81723ff823a5e695cbbfd5be0228783958cd4148
Dale Sublett <djsublett04@gmail.com>
2019-03-27 00:36:36 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -32,7 +32,7 @@ function destructivelyAddElementToEndOfArray(array, element) {
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift();
-  array
+  return array
 }
 
 function removeElementFromBeginningOfArray(array) {
